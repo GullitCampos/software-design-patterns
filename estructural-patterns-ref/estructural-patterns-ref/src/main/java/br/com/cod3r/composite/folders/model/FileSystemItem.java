6e6302696e0e6142b09ae1d59708fd1a2255c909
Gullit Damião Teixeira de Campos <155154190+GullitCampos@users.noreply.github.com>
2025-07-18 01:52:17 -0300

    Design Patterns

--- estructural-patterns-ref/estructural-patterns-ref/src/main/java/br/com/cod3r/composite/folders/model/FileSystemItem.java
+++
@@ -1,5 +0,0 @@
-package br.com.cod3r.composite.folders.model;
-
-public interface FileSystemItem {
-	void print(String structure);
-}
--- estructural-patterns-ref/estructural-patterns-ref/src/main/java/br/com/cod3r/mediator/swing/after/Client.java
+++
@@ -1,9 +0,0 @@
-package br.com.cod3r.mediator.swing.after;
-
-public class Client {
-	
-	public static void main(String[] args) {
-		Screens screens = new Screens();
-		screens.init();
-	}
-}
